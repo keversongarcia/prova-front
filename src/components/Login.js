27c1d480f14a1b44f94c6cd7bafd6e4dc39dd9ab
rabello97 <rodrigo.azevedo.rabello@gmail.com>
2022-03-06 16:42:30 -0300

    up css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,26 @@
+import "../components/login.css";
 import React from "react";
 
 const Login = () => {
-  return <div className=".login">Login</div>;
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="login-banner">
+          <h1 className="h1-banner">Iquefome!</h1>
+        </div>
+        <div className="login-box">
+          <h1 className="h1-login">Login</h1>
+          <form>
+            <label>Usuário: </label>
+            <input></input>
+            <label>Senha: </label>
+            <input></input>
+            <button>Entrar</button>
+          </form>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Login;
